5554ffa51022d845dd462b3ba37d77624c03235d
anouri <dynamic11@gmail.com>
2016-06-05 00:43:28 -0400

    formatting shopping cart/home/navbar

--- resources/views/cart/shoppingCart.blade.php
+++ resources/views/cart/shoppingCart.blade.php
@@ -4,16 +4,24 @@
 <div class="container">
     <div class="row">
         <div class="col-md-9">
-            <ul>
-                @foreach($booksInCart as $bookInCart)
-                    <li>{{$bookInCart->book->book_name}}---->{{$bookInCart->book->book_price}}</li>
-                        <form method="POST" action="/removebookfromcart">
-                            {!! csrf_field() !!}
-                            <input type="hidden" name="book_id" value="{{$bookInCart->id}}">
-                            <button type="submit" class="btn btn-danger">Remove From Cart</button>
-                        </form>
-                @endforeach
-            </ul>
+        <h1>Shopping Cart</h1>
+            @if ($booksInCart->isEmpty())
+                    <p>cart is empty</p>
+                    <a href="/preorderbook" class="btn btn-primary">+ Add Book To Cart</a>
+                @else
+                    <ul>
+                        @foreach($booksInCart as $bookInCart)
+                            <li>{{$bookInCart->book->book_name}}---->{{$bookInCart->book->book_price}}</li>
+                                <form method="POST" action="/removebookfromcart">
+                                    {!! csrf_field() !!}
+                                    <input type="hidden" name="book_id" value="{{$bookInCart->id}}">
+                                    <button type="submit" class="btn btn-danger">Remove From Cart</button>
+                                </form>
+                        @endforeach
+                    </ul>
+                    <a href="/checkout" class="btn btn-primary">Procees To Checkout</a>
+                    <a href="/preorderbook" class="btn btn-primary">+ Add Another Book To Cart</a>
+                @endif
         </div>
         <div class="col-md-3">
             <ul>
@@ -27,8 +35,6 @@
                 <?php echo("<p>Total: $". $totalPrice ."</p>") ?>
             </ul>
         </div>
-        <a href="/checkout" class="btn btn-primary">Procees To Checkout</a>
-        <a href="/preorderbook" class="btn btn-primary">+ Add Another Book To Cart</a>
     </div>
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <a href="{{url('preorderbook')}}">Preorder TextBook</a></br>
-            <a href="#"> Buy Others</a></br>
             <a href="{{url('orderstatus')}}"> Order PickUp/Status</a>
         </div>
     </div>
