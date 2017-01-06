10b9d937727a512b73b035bff81d197df5665988
alinouri <alinouri@dhcp-35-6.cu-wireless-faculty.carleton.ca>
2017-01-06 13:25:40 -0500

    added icon to dashboard lite

--- resources/views/adminDash/dashboardLite.blade.php
+++ resources/views/adminDash/dashboardLite.blade.php
@@ -1,10 +1,30 @@
-@extends('layouts.app')
+<style>
+.btn-sq-lg {
+  width: 150px !important;
+  height: 150px !important;
+  padding-top: 30px !important;
+  margin:10px;
+}
 
+.btn-sq-lg i{
+  padding-bottom: 10px !important;
+}
+
+.btn-sq-lg p{
+  line-height: 15px !important;
+}
+
+</style>
+
+@extends('layouts.app')
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-        	<a href="/ordermonitoring">View/Deliver Orders</a>
+        <div class="col-md-10">
+        	<a href="/ordermonitoring" class="btn btn-sq-lg btn-primary">
+                <i class="fa fa-database fa-5x" aria-hidden="true"></i></br>
+                <p>View / Deliver</br>Orders</p>
+            </a>
         </div>
         
     </div>
