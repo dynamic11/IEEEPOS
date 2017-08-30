7f926f6c75937876e2066cb0b3f25183be3a0485
dynamic11 <dynamic11@gmail.com>
2017-08-30 19:04:26 -0400

    added button to pay with square

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -19,45 +19,69 @@
 
             <fieldset class="form-group">
                 <label for="exampleSelect1">Payment Method</label>
-                <select name= "payment_type" class="form-control"  id="exampleSelect1">
+                <select name= "payment_type" class="form-control"  id="paymentOption">
                   <option value="">---Select---</option>
                   <option value="cash">Cash</option>
-                  <option value="Square">Square</option>
+                  <option value="square">Square</option>
                 </select>
                 @if ($errors->has('payment_type')) <p class="help-block" style="color:red">{{ $errors->first('payment_type') }}</p> @endif
-                <div class="checkbox">
+                <div class="checkbox" id="cashConfirm" style="display:none">
                     <label>
                       <input name= "confirm_payment" type="checkbox"> I have collected the money for this order
                     </label>
                     @if ($errors->has('confirm_payment')) <p class="help-block" style="color:red">{{ $errors->first('confirm_payment') }}</p> @endif
                 </div>
             </fieldset>
-            <button type="submit" class="btn btn-primary">Complete Purchase</button>
+            <button type="submit" class="btn btn-primary" id="cashButton" style="display:none"><i class="fa fa-money" aria-hidden="true"></i> Pay With Cash</button>
+            
             </form>
-
-   <button  id="squareButton" class="btn btn-primary">Square</button>
+            <button type="submit" class="btn btn-primary" id="disabledButton" style="display:inline; background-color:grey">Please Select payment method</button>
+            <button type="submit" class="btn btn-primary" id="squareButton" style="display:none"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Pay With Square</button>
         </div>
         
 
     </div>
 
     <script>
-    document.getElementById("squareButton").addEventListener("click", function(){
-        var dataParameter = {
-            "amount_money": {
-              "amount" : "25",
-              "currency_code" : "CAD"
-            },
-            "callback_url" : "http://159.203.23.90", // Replace this value with your application's callback URL
-            "client_id" : "sandbox-sq0idp-uPsZe4Id6AHglQEhpZwx3A", // Replace this value with your application's ID
-            "version": "1.3",
-            "notes": "notes for the transaction",
-            "options" : {
-              "supported_tender_types" : ["CREDIT_CARD","CASH","OTHER","SQUARE_GIFT_CARD","CARD_ON_FILE"]
-            }
-          };
-      window.location = "square-commerce-v1://payment/create?data=" + encodeURIComponent(JSON.stringify(dataParameter));
-    });
+
+        $(document).ready(function(){
+            $("#paymentOption").change(function () {
+                var selectedPaymentMethod = $(this).val();
+                if (selectedPaymentMethod == "cash") {
+                    document.getElementById("disabledButton").style.display = "none"
+                    document.getElementById("cashButton").style.display = "inline"
+                    document.getElementById("squareButton").style.display = "none"
+                    document.getElementById("cashConfirm").style.display = "inline"
+                }else if(selectedPaymentMethod == "square"){
+                    document.getElementById("disabledButton").style.display = "none"
+                    document.getElementById("cashButton").style.display = "none"
+                    document.getElementById("squareButton").style.display = "inline"
+                    document.getElementById("cashConfirm").style.display = "none"
+                }else{
+                    document.getElementById("disabledButton").style.display = "inline"
+                    document.getElementById("cashButton").style.display = "none"
+                    document.getElementById("squareButton").style.display = "none"
+                    document.getElementById("cashConfirm").style.display = "none"
+                }
+            });
+        });
+
+        document.getElementById("squareButton").addEventListener("click", function(){
+            var dataParameter = {
+                "amount_money": {
+                  "amount" : "25",
+                  "currency_code" : "CAD"
+                },
+                "callback_url" : "https://ieeecarletonvolunteer.xyz", // Replace this value with your application's callback URL
+                "client_id" : "sandbox-sq0idp-uPsZe4Id6AHglQEhpZwx3A", // Replace this value with your application's ID
+                "version": "1.3",
+                "notes": "notes for the transaction",
+                "options" : {
+                  "supported_tender_types" : ["CREDIT_CARD","CASH","OTHER","SQUARE_GIFT_CARD","CARD_ON_FILE"]
+                }
+              };
+          window.location = "square-commerce-v1://payment/create?data=" + encodeURIComponent(JSON.stringify(dataParameter));
+        });
     </script>
 </div>
 @endsection
